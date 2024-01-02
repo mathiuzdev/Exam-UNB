52a445f7347173bc8c887db198f1fff66894249e
Matias Ortega <matias.pedro.ortega@gmail.com>
2024-01-02 11:29:22 -0300

    config vite add

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            base: 'https://exam-unb-production.up.railway.app/',
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
